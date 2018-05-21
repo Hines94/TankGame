3264027760b14a85d0164167bf2086168be3d753
DESKTOP-QROFTE7\jack- <secretguy1919@gmail.com>
2018-05-21 21:30:29 +0100

    Started to build the tank class and player tank controller. The tank logs out to show whom is posessed. Main menu level included.

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller not posessing a tank"));
+	}
+
+
+	UE_LOG(LogTemp, Warning, TEXT("Controlled Tank is: %s"), *(ControlledTank->GetName()));
+
+}
+
+ATank * ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+	
+}
